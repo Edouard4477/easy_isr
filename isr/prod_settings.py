8f49ce11489f889119a01a77d38e8fe14cde8f73
Dre <edouard.mercier@sgra-captives.com>
2018-07-13 00:26:27 +0200

    make it better

--- isr/prod_settings.py
+++ isr/prod_settings.py
@@ -6,6 +6,9 @@ TEMPLATE_DEBUG = True
 
 DATABASES['default'] = dj_database_url.config()
 
+MIDDLEWARE +=['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 SECRET_KEY = '@yb&tp72ydnre^jt&-72ag&m@85f&mlg0xa^u)^_5$wt1_3e%m'
 
 ALLOWED_HOSTS = ['easy-isr.herokuapp.com']
