5239d68207ce98119617000aff5036fc0f14b05a
Dre <edouard.mercier@sgra-captives.com>
2018-07-12 23:12:06 +0200

    make it better

--- isr/prod_settings.py
+++ isr/prod_settings.py
@@ -1,7 +1,10 @@
+import dj_database_url
 from .settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
 
 SECRET_KEY = '@yb&tp72ydnre^jt&-72ag&m@85f&mlg0xa^u)^_5$wt1_3e%m'
 
